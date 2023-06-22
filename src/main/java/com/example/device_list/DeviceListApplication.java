c869424fbbbbb6093e85af3f8bb722c07c68f233
Akv0603 <akv060388@gmail.com>
2023-06-22 11:09:18 +0700

    Initial commit

---
+++ src/main/java/com/example/device_list/DeviceListApplication.java
@@ -0,0 +1,13 @@
+package com.example.device_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/device_list/DeviceListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.device_list;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
